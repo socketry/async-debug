564e9d508840833ad40e33bcdb624a45f21ba420
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-05-14 16:36:06 +1200

    Remove superfluous logging.

--- pages/controller.rb
+++ pages/controller.rb
@@ -7,8 +7,6 @@ prepend Actions
 RESOLVER = Live::Resolver.allow(Async::Debug::ReactorView)
 
 on 'live' do |request|
-	Console.logger.info("Incoming live connection...")
-	
 	adapter = Async::WebSocket::Adapters::Rack.open(request.env) do |connection|
 		Live::Page.new(RESOLVER).run(connection)
 	end
